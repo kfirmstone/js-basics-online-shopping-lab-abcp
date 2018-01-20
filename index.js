77f993f599a295a6dacde76336ea084171bb7f95
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-01-20 19:48:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,17 +3,34 @@ var cart = [];
 function getCart() {
  return cart;
 }
-
 function setCart(c) {
   cart = c;
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+   
+  var itemPrice = Math.floor(Math.random()*100)
+  var newItem = {}
+  newItem[itemName]=itemPrice
+cart.push(newItem)
+console.log (`${itemName} has been added to your cart.`)
+return cart
 }
 
 function viewCart() {
+  if (cart.length===0){
+    console.log (`Your shopping cart is empty.`)
+  }else if(cart.length===1){
+    for(var i=0; i<cart.length; i++)
+    console.log(`In your cart, ${cart[i]} you have  at ${cart.i}.`)
+  }else if (cart.length===2){
+    for (i=0; i<cart.length; i++)
+    console.log(`In your cart, you have ${Object.keys(cart)} at $${Object.entries(cart)} and ${Object.keys(cart)} at ${Object.values(cart)}`)
+  }else (cart.lenghth>2);{
+    for (i=0; i<cart.length; i++)
+    console.log(`In your cart, you have ${Object.keys(cart)} at $${Object.entries(cart)}, ${Object.keys(cart)}at $${Object.entries(cart)}, and ${Object.keys(cart)} at $${Object.entries(cart)}.`) 
+  }
   // write your code here
 }
 
